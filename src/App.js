7290efd5ba375c96c999c74ee91c559db1a21541
Mohamed Abassia <mo3bassia@gmail.com>
2024-10-07 19:22:38 +0300

    add react static components

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./index.css";
 
 const initialFriends = [
@@ -22,5 +23,100 @@ const initialFriends = [
 ];
 
 export default function App() {
-  return <h1>Hello</h1>;
+  return (
+    <div className="app">
+      <aside className="sidebar">
+        <Friends />
+        <AddFriend />
+        <Button>Add Friend</Button>
+      </aside>
+      <SplitBill />
+    </div>
+  );
+}
+
+function SplitBill() {
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+  return (
+    <form className="form-split-bill" onSubmit={handleSubmit}>
+      <h2>SPLIT A BILL WITH ANTHONY</h2>
+      <label htmlFor="bill">💰 Bill value</label>
+      <input type="number" id="bill"></input>
+      <label htmlFor="yourExpense">🧍‍♀️ Your expense</label>
+      <input type="number" id="yourExpense"></input>
+      <label htmlFor="anthonyExpense">👫 Anthony's expense</label>
+      <input type="number" id="anthonyExpense" readOnly disabled></input>
+      <label htmlFor="selectBox">🤑 Who is paying the bill</label>
+      <select id="selectBox">
+        <option value="you">You</option>
+        <option value="Sarah">Sarah</option>
+      </select>
+      <Button>Split Bill</Button>
+    </form>
+  );
+}
+
+function Friends() {
+  return (
+    <ul>
+      {initialFriends.map((friend) => (
+        <Friend
+          key={friend.id}
+          id={friend.id}
+          name={friend.name}
+          image={friend.image}
+          balance={friend.balance}
+        />
+      ))}
+    </ul>
+  );
+}
+
+function AddFriend() {
+  const [url, setUrl] = useState("https://i.pravatar.cc/48");
+  return (
+    <form className="form-add-friend">
+      <label htmlFor="friendName">👫 Friend name</label>
+      <input id="friendName"></input>
+      <label htmlFor="imageUrl">🌄 Image URL</label>
+      <input
+        type="url"
+        id="imageUrl"
+        value={url}
+        onChange={(e) => setUrl(e.target.value)}
+      ></input>
+      <Button>Add</Button>
+    </form>
+  );
+}
+
+function Friend({ id, name, image, balance }) {
+  return (
+    <li>
+      <img src={image} alt={name}></img>
+      <h3>{name}</h3>
+      {balance > 0 ? (
+        <p className="green">
+          {name} owes you {balance}€
+        </p>
+      ) : balance === 0 ? (
+        <p>You and {name} are even</p>
+      ) : (
+        <p className="red">
+          You owe {name} {balance}€
+        </p>
+      )}
+      <Button>Select</Button>
+    </li>
+  );
+}
+
+function Button({ onClick, children, onSubmiting }) {
+  return (
+    <button className="button" onClick={onClick} onSubmit={onSubmiting}>
+      {children}
+    </button>
+  );
 }
